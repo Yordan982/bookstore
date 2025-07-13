50474adf2de56159b95cefe80e5045ff63936bc5
yordan98 <penguinn2@mail.bg>
2025-07-13 19:55:34 +0300

    Added User controller

---
+++ src/main/java/com/monevia/bookstore/user_service/UserController.java
@@ -0,0 +1,41 @@
+package com.monevia.bookstore.user_service;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Map<String, String>> createUser(
+            @RequestBody
+            @Validated CreateUserDTO createUserDTO) {
+        String newUserId = userService.createUser(createUserDTO);
+        Map<String, String> responseBody = Map.of("user_id", newUserId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
+    }
+
+    @PatchMapping("/{user_id}/update")
+    public ResponseEntity<Map<String, String>> updateUser(
+            @PathVariable("user_id") String userId,
+            @RequestBody @Valid UpdateUserDTO updateUserDTO) {
+        userService.updateUser(userId, updateUserDTO);
+        Map<String, String> response = Map.of(
+                "message", UserConstants.USER_UPDATED,
+                "user_id", userId,
+                "name", updateUserDTO.getName());
+        return ResponseEntity.ok(response);
+    }
+}
