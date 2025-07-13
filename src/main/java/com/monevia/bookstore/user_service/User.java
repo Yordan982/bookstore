31db4f9113f6436e6dac3b2095ea3c8bc2d709e4
yordan98 <penguinn2@mail.bg>
2025-07-13 19:52:56 +0300

    Added User entity class

---
+++ src/main/java/com/monevia/bookstore/user_service/User.java
@@ -0,0 +1,64 @@
+package com.monevia.bookstore.user_service;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(length = 36, nullable = false, unique = true)
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+
+    public User() {
+    }
+
+    public User(String name, String email, String address) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public User setId(String customerId) {
+        this.id = customerId;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public User setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+}
