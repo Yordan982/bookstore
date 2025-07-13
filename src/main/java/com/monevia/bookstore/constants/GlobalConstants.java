bebd844ccb49247782c796ad166cb0b140e9a9a7
yordan98 <penguinn2@mail.bg>
2025-07-13 14:19:33 +0300

    Added global constants

---
+++ src/main/java/com/monevia/bookstore/constants/GlobalConstants.java
@@ -0,0 +1,6 @@
+package com.monevia.bookstore.constants;
+
+public class GlobalConstants {
+    public static final String INVALID_PARAMETER = "Invalid parameters provided: ";
+    public static final String MESSAGE = "message";
+}
