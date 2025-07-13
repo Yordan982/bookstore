9e04828827d55d0ce27a2c9bcb6b886fb5aa98a3
yordan98 <penguinn2@mail.bg>
2025-07-13 14:19:52 +0300

    Added error handler

---
+++ src/main/java/com/monevia/bookstore/errors/ErrorHandler.java
@@ -0,0 +1,37 @@
+package com.monevia.bookstore.errors;
+
+import com.monevia.bookstore.constants.GlobalConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleInvalidEnum(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(GlobalConstants.INVALID_PARAMETER + ex.getMessage());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
+        return ResponseEntity
+                .status(ex.getStatusCode())
+                .body(Map.of(GlobalConstants.MESSAGE, Objects.requireNonNull(ex.getReason())));
+    }
+}
