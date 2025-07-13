b2fabaa94821448ee108cc3c6d80aac8c6de648c
yordan98 <penguinn2@mail.bg>
2025-07-13 19:54:16 +0300

    Added User constants

---
+++ src/main/java/com/monevia/bookstore/user_service/UserConstants.java
@@ -0,0 +1,13 @@
+package com.monevia.bookstore.user_service;
+
+public class UserConstants {
+    public static final String NAME_IS_REQUIRED = "Name is required";
+    public static final String NAME_MAX_LENGTH = "Name must be {max} characters or fewer";
+    public static final String EMAIL_IS_REQUIRED = "Email is required";
+    public static final String EMAIL_IS_INVALID = "Email is not valid";
+    public static final String EMAIL_VALIDATION_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    public static final String ADDRESS_IS_REQUIRED = "Address is required";
+    public static final String ADDRESS_MAX_LENGTH = "Address must be {max} characters or fewer";
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String USER_UPDATED = "User updated successfully";
+}
