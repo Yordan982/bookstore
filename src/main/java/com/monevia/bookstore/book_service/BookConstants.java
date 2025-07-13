f361def5f2002a9abeb7011373e9e3b1f51e7fed
yordan98 <penguinn2@mail.bg>
2025-07-13 14:17:21 +0300

    Added Book constants

---
+++ src/main/java/com/monevia/bookstore/book_service/BookConstants.java
@@ -0,0 +1,13 @@
+package com.monevia.bookstore.book_service;
+
+public class BookConstants {
+    public static final String TITLE_IS_REQUIRED = "Title is required";
+    public static final String TITLE_MAX_LENGTH = "Title must be {max} characters or fewer";
+    public static final String AUTHOR_IS_REQUIRED = "Author is required";
+    public static final String AUTHOR_MAX_LENGTH = "Author must be {max} characters or fewer";
+    public static final String PRICE_IS_REQUIRED = "Price is required";
+    public static final String PRICE_NOT_POSITIVE = "Price must be zero or positive";
+    public static final String GENRE_IS_REQUIRED = "Genre is required";
+    public static final String BOOK_NOT_FOUND = "Book not found";
+    public static final String BOOK_DELETED = "The book was deleted successfully";
+}
