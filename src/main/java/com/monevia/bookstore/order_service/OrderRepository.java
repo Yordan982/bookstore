ac469399dfc440039402cfaeb28e1b41a3410fad
yordan98 <penguinn2@mail.bg>
2025-07-14 23:22:25 +0300

    Added method to find books in an Order

--- src/main/java/com/monevia/bookstore/order_service/OrderRepository.java
+++ src/main/java/com/monevia/bookstore/order_service/OrderRepository.java
@@ -1,8 +1,15 @@
 package com.monevia.bookstore.order_service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
+
+    @Query(value = "SELECT book_id FROM order_books WHERE order_id = :orderId", nativeQuery = true)
+    List<String> findBookIdsByOrderId(@Param("orderId") String orderId);
 }
