b976e6cb94e60d44754a5aec9ec4a08b588ba49c
yordan98 <penguinn2@mail.bg>
2025-07-14 09:54:57 +0300

    Added Order constants

---
+++ src/main/java/com/monevia/bookstore/order_service/OrderConstants.java
@@ -0,0 +1,13 @@
+package com.monevia.bookstore.order_service;
+
+public class OrderConstants {
+    public static final String BOOK_IDS_IS_REQUIRED = "The order must contain at least one valid book";
+    public static final String TOTAL_AMOUNT_IS_REQUIRED = "Total amount is required";
+    public static final String TOTAL_AMOUNT_NOT_POSITIVE = "Total amount must be zero or positive";
+    public static final String STATUS_IS_REQUIRED = "Status is required";
+    public static final String STATUS_NOT_VALID = "Status is not valid";
+    public static final String ORDER_CREATED = "Order created successfully";
+    public static final String ORDER_NOT_FOUND = "Order not found";
+    public static final String ORDER_UPDATED = "Order updated successfully";
+    public static final String ORDER_DELETED = "Order deleted successfully";
+}
