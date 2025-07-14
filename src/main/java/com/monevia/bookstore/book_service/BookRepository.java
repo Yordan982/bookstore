a2a94d706956204893c854844af46be798845d8f
yordan98 <penguinn2@mail.bg>
2025-07-14 09:51:13 +0300

    Added method to access books by id.

--- src/main/java/com/monevia/bookstore/book_service/BookRepository.java
+++ src/main/java/com/monevia/bookstore/book_service/BookRepository.java
@@ -3,6 +3,11 @@ package com.monevia.bookstore.book_service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
+    List<Book> streamBooksById(String id);
+
+    Iterable<String> getBooksById(String id);
 }
