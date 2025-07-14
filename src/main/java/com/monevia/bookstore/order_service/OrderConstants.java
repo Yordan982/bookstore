a85fcd5eb5d74fa62a3234d2b478b6102c652d7d
yordan98 <penguinn2@mail.bg>
2025-07-14 22:01:03 +0300

    Added missing customer constant

--- src/main/java/com/monevia/bookstore/order_service/OrderConstants.java
+++ src/main/java/com/monevia/bookstore/order_service/OrderConstants.java
@@ -7,6 +7,7 @@ public class OrderConstants {
     public static final String STATUS_IS_REQUIRED = "Status is required";
     public static final String STATUS_NOT_VALID = "Status is not valid";
     public static final String CUSTOMER_ID_IS_REQUIRED = "Customer id is required";
+    public static final String CUSTOMER_ID_NOT_FOUND = "Customer id does not exist";
     public static final String ORDER_CREATED = "Order created successfully";
     public static final String ORDER_NOT_FOUND = "Order not found";
     public static final String ORDER_UPDATED = "Order updated successfully";
