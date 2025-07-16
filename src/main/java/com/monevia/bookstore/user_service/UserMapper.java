28d51d851d4693c802614479a09d5dff5c27bd00
yordan98 <penguinn2@mail.bg>
2025-07-16 10:21:11 +0300

    Added User mapper for the ability to update only specified fields

---
+++ src/main/java/com/monevia/bookstore/user_service/UserMapper.java
@@ -0,0 +1,12 @@
+package com.monevia.bookstore.user_service;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateUserFromDTO(UpdateUserDTO dto, @MappingTarget User entity);
+}
