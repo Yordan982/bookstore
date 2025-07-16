f720d373e5ec43f6ced7f54b320142bb57501fee
yordan98 <penguinn2@mail.bg>
2025-07-16 21:04:26 +0300

    Added a constant when a user is deleted

--- src/main/java/com/monevia/bookstore/user_service/UserConstants.java
+++ src/main/java/com/monevia/bookstore/user_service/UserConstants.java
@@ -10,4 +10,5 @@ public class UserConstants {
     public static final String ADDRESS_MAX_LENGTH = "Address must be {max} characters or fewer";
     public static final String USER_NOT_FOUND = "User not found";
     public static final String USER_UPDATED = "User updated successfully";
+    public static final String USER_DELETED = "User deleted successfully";
 }
