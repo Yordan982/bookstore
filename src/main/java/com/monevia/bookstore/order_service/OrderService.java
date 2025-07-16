fbb0e8ba4efd94be6177c8528915c0174cf691c3
yordan98 <penguinn2@mail.bg>
2025-07-16 22:30:07 +0300

    Added a method to update order and refactored existing methods for the creation of the order

--- src/main/java/com/monevia/bookstore/order_service/OrderService.java
+++ src/main/java/com/monevia/bookstore/order_service/OrderService.java
@@ -3,7 +3,9 @@ package com.monevia.bookstore.order_service;
 import com.monevia.bookstore.book_service.Book;
 import com.monevia.bookstore.book_service.BookRepository;
 import com.monevia.bookstore.user_service.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -13,11 +15,13 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final BookRepository bookRepository;
     private final UserRepository userRepository;
+    private final OrderMapper orderMapper;
 
-    public OrderService(OrderRepository orderRepository, BookRepository bookRepository, UserRepository userRepository) {
+    public OrderService(OrderRepository orderRepository, BookRepository bookRepository, UserRepository userRepository, OrderMapper bookMapper) {
         this.orderRepository = orderRepository;
         this.bookRepository = bookRepository;
         this.userRepository = userRepository;
+        this.orderMapper = bookMapper;
     }
 
     public Order createOrder(CreateOrderDTO createOrderDTO) {
@@ -32,22 +36,8 @@ public class OrderService {
                 .map(Book::getId)
                 .toList();
         BigDecimal totalAmount = calculateTotalAmount(foundBooks);
-        Order order = createNewOrder(createOrderDTO.getCustomerId(), validBookIds, totalAmount);
+        Order order = new Order(createOrderDTO.getCustomerId(), validBookIds, totalAmount, OrderStatus.CREATED);
         return orderRepository.save(order);
-    }
-
-    private Order createNewOrder(String customerId, List<String> bookIds, BigDecimal totalAmount) {
-        return new Order(customerId, bookIds, totalAmount, OrderStatus.CREATED);
-    }
-
-    private BigDecimal calculateTotalAmount(List<Book> books) {
-        return books.stream()
-                .map(book -> BigDecimal.valueOf(book.getPrice()))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
-    private boolean userExists(String userId) {
-        return userRepository.existsById(userId);
     }
 
     public GetOrderDTO getOrder(String orderId) {
@@ -55,5 +45,35 @@ public class OrderService {
                 new IllegalArgumentException(OrderConstants.ORDER_NOT_FOUND));
         List<String> booksIds = orderRepository.findBookIdsByOrderId(orderId);
         return new GetOrderDTO(order.getCustomerId(), booksIds, order.getTotalAmount(), order.getStatus());
+    }
+
+    public void updateOrder(String orderId, UpdateOrderDTO updateOrderDTO) {
+        Order order = orderRepository.findById(orderId).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        OrderConstants.ORDER_NOT_FOUND));
+        if (updateOrderDTO.getBookIds() != null && !updateOrderDTO.getBookIds().isEmpty()) {
+            List<Book> foundBooks = bookRepository.findAllById(updateOrderDTO.getBookIds());
+            List<String> validBookIds = foundBooks.stream()
+                    .map(Book::getId)
+                    .toList();
+            updateOrderDTO.setBookIds(validBookIds);
+            if (updateOrderDTO.getTotalAmount() == null) {
+                updateOrderDTO.setTotalAmount(calculateTotalAmount(foundBooks));
+            }
+            order.getBookIds().clear();
+            order.getBookIds().addAll(validBookIds);
+        }
+        orderMapper.updateOrderFromDTO(updateOrderDTO, order);
+        orderRepository.save(order);
+    }
+
+    private boolean userExists(String userId) {
+        return userRepository.existsById(userId);
+    }
+
+    private BigDecimal calculateTotalAmount(List<Book> books) {
+        return books.stream()
+                .map(book -> BigDecimal.valueOf(book.getPrice()))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
