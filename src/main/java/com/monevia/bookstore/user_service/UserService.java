5dbb57ee0253be0fe6639be2ae91f66e2d92b223
yordan98 <penguinn2@mail.bg>
2025-07-17 21:54:12 +0300

    Added password encryption when creating and updating users

--- src/main/java/com/monevia/bookstore/user_service/UserService.java
+++ src/main/java/com/monevia/bookstore/user_service/UserService.java
@@ -1,6 +1,7 @@
 package com.monevia.bookstore.user_service;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -8,14 +9,16 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public String createUser(CreateUserDTO userDTO) {
-        User user = new User(userDTO.getName(), userDTO.getEmail().toLowerCase(), userDTO.getPassword(), userDTO.getAddress());
+        User user = new User(userDTO.getName(), userDTO.getEmail().toLowerCase(), encodePassword(userDTO.getPassword()), userDTO.getAddress());
         userRepository.save(user);
         return user.getId();
     }
@@ -24,6 +27,9 @@ public class UserService {
         User user = userRepository.findById(userId).orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.NOT_FOUND,
                         UserConstants.USER_NOT_FOUND));
+        if (updateUserDTO.getPassword() != null) {
+            updateUserDTO.setPassword(encodePassword(updateUserDTO.getPassword()));
+        }
         userMapper.updateUserFromDTO(updateUserDTO, user);
         userRepository.save(user);
     }
@@ -35,5 +41,12 @@ public class UserService {
         }
         userRepository.deleteById(userId);
         return UserConstants.USER_DELETED;
+    }
+
+    public String encodePassword(String password) {
+        if (!password.matches(UserConstants.PASSWORD_VALIDATION_REGEX)) {
+            throw new IllegalArgumentException(UserConstants.PASSWORD_INVALID);
+        }
+        return passwordEncoder.encode(password);
     }
 }
