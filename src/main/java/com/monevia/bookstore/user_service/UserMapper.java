41260d96b3180318818da07a549e8795b0b05133
yordan98 <penguinn2@mail.bg>
2025-07-17 21:05:54 +0300

    Added mapping for the password in the UserMapper

--- src/main/java/com/monevia/bookstore/user_service/UserMapper.java
+++ src/main/java/com/monevia/bookstore/user_service/UserMapper.java
@@ -1,12 +1,11 @@
 package com.monevia.bookstore.user_service;
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", source = "password", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateUserFromDTO(UpdateUserDTO dto, @MappingTarget User entity);
 }
