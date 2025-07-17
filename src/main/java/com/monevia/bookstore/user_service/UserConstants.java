f8f81f7dc0087f5e800952ec9b63168c8f8f0a66
yordan98 <penguinn2@mail.bg>
2025-07-17 21:05:15 +0300

    Added password regex and validation error message

--- src/main/java/com/monevia/bookstore/user_service/UserConstants.java
+++ src/main/java/com/monevia/bookstore/user_service/UserConstants.java
@@ -6,6 +6,8 @@ public class UserConstants {
     public static final String EMAIL_IS_REQUIRED = "Email is required";
     public static final String EMAIL_IS_INVALID = "Email is not valid";
     public static final String EMAIL_VALIDATION_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    public static final String PASSWORD_VALIDATION_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{10,50}$";
+    public static final String PASSWORD_INVALID = "Password must be 10-50 characters long and include at least one uppercase letter, one digit, and one special character";
     public static final String ADDRESS_IS_REQUIRED = "Address is required";
     public static final String ADDRESS_MAX_LENGTH = "Address must be {max} characters or fewer";
     public static final String USER_NOT_FOUND = "User not found";
