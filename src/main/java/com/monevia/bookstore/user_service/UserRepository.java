fad59d2d3aed3dd6d3db5e14d38a9a64c9355215
yordan98 <penguinn2@mail.bg>
2025-07-18 22:59:31 +0300

    Added User repository method to check if a user with the same email already exists

--- src/main/java/com/monevia/bookstore/user_service/UserRepository.java
+++ src/main/java/com/monevia/bookstore/user_service/UserRepository.java
@@ -3,6 +3,10 @@ package com.monevia.bookstore.user_service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByEmail(String email);
+
+    boolean existsByEmailAndIdNot(String email, String id);
 }
