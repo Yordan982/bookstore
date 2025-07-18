e405884ed67c28d95cf62c1185eb2c652044635d
yordan98 <penguinn2@mail.bg>
2025-07-18 21:43:59 +0300

    Removed totalAmount and status from CreateOrderDTO as they are respectively calculated based on the bookIds and have default status upon creation.

--- src/main/java/com/monevia/bookstore/order_service/CreateOrderDTO.java
+++ src/main/java/com/monevia/bookstore/order_service/CreateOrderDTO.java
@@ -1,11 +1,9 @@
 package com.monevia.bookstore.order_service;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -15,11 +13,6 @@ public class CreateOrderDTO {
 
     @JsonProperty("book_ids")
     private List<String> bookIds;
-
-    @Column(name = "total_amount")
-    private BigDecimal totalAmount;
-
-    private OrderStatus status;
 
     public CreateOrderDTO(List<String> bookIds) {
         this.bookIds = bookIds;
