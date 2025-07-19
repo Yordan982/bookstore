746d9e2f6810233f9c62a73b61671bc945a487b5
yordan98 <penguinn2@mail.bg>
2025-07-19 15:27:28 +0300

    Added constants for quantity

--- src/main/java/com/monevia/bookstore/book_service/BookConstants.java
+++ src/main/java/com/monevia/bookstore/book_service/BookConstants.java
@@ -8,7 +8,11 @@ public class BookConstants {
     public static final String PRICE_IS_REQUIRED = "Price is required";
     public static final String PRICE_NOT_POSITIVE = "Price must be zero or positive";
     public static final String GENRE_IS_REQUIRED = "Genre is required";
-    public static final String INSUFFICIENT_QUANTITY = "Insufficient quantity: only %s available";
+    public static final String QUANTITY_NOT_POSITIVE = "Quantity must be greater than or equal to zero";
+    public static final String QUANTITY_UPDATED = "Quantity updated successfully to %d";
+    public static final String QUANTITY_ADDED = "Quantity has been increased by %d";
+    public static final String QUANTITY_REMOVED = "Quantity has been decreased by %d";
+    public static final String QUANTITY_INSUFFICIENT = "Insufficient quantity: only %s available";
     public static final String BOOK_CREATED = "Book created successfully";
     public static final String BOOK_UPDATED = "Book updated successfully";
     public static final String BOOK_DELETED = "Book deleted successfully";
