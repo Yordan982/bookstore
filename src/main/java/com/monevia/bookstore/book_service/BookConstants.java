b505aaed72b7a684a9c191b26fd5f8b5aaa0471d
yordan98 <penguinn2@mail.bg>
2025-07-19 11:53:26 +0300

    Added new Book Constants for Book quantity and message upon creation

--- src/main/java/com/monevia/bookstore/book_service/BookConstants.java
+++ src/main/java/com/monevia/bookstore/book_service/BookConstants.java
@@ -8,7 +8,10 @@ public class BookConstants {
     public static final String PRICE_IS_REQUIRED = "Price is required";
     public static final String PRICE_NOT_POSITIVE = "Price must be zero or positive";
     public static final String GENRE_IS_REQUIRED = "Genre is required";
-    public static final String BOOK_NOT_FOUND = "Book not found";
+    public static final String INSUFFICIENT_QUANTITY = "Insufficient quantity: only %s available";
+    public static final String BOOK_CREATED = "Book created successfully";
     public static final String BOOK_UPDATED = "Book updated successfully";
     public static final String BOOK_DELETED = "Book deleted successfully";
+    public static final String BOOK_NOT_FOUND = "Book not found";
+    public static final String BOOK_INVENTORY_NOT_FOUND = "Book inventory not found";
 }
